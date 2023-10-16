245d3e9407d520a31aab28ffd2ade1f926aff357
ilkay <06ilkayozturk@gmail.com>
2023-10-16 19:08:04 +0300

    controllers:users

--- index.js
+++ index.js
@@ -23,8 +23,27 @@ dbConnection();
 /* --------------------------------------------- */
 // Middlewares:
 
+// Accept JSON:
+app.use(express.json());
+
+// Run Logger:
+app.use(require("./src/middlewares/logger"));
+
+// res.getModelList():
+app.use(require("./src/middlewares/findSearchSortPage"));
+
 /* --------------------------------------------- */
 // Routes:
+
+// HomePath:
+app.all("/", (req, res) => {
+  res.send({
+    error: false,
+    message: "Welcome to PIZZA API",
+    isLogin: req.isLogin,
+    user: req.user,
+  });
+});
 
 /* --------------------------------------------- */
 
---
+++ src/controllers/user.js
@@ -0,0 +1,13 @@
+"use strict";
+/* ----------------------
+    NODEJS EXPRESS | PIZZA API
+---------------------------------- */
+// User Controller:
+module.exports = {
+  list: async (req, res) => {},
+  create: async (req, res) => {},
+  read: async (req, res) => {},
+  update: async (req, res) => {},
+  delete: async (req, res) => {},
+};
+/* ----------------------------------------------------- */
